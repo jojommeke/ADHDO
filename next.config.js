3caa53278de27e7756440b40e1bfff6b16be37f5
Joran Hennion <joran.hennion@gmail.com>
2024-09-29 19:41:32 +0200

    first github page build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/adhdo,
+    basePath: "/adhdo",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
