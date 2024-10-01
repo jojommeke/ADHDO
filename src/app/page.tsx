fb55afc5070f4168a700a58d3279ea2f5629e7cc
Joran Hennion <joran.hennion@gmail.com>
2024-10-01 21:26:52 +0200

    components yay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,29 @@ const kranky = Kranky({
 })
 
 function Action(props) {
-  return <div id={props.id} className={"size-12 rounded-lg flex justify-center flex-col" + " " + "bg-[" + props.color + "]"}>
+  return <div id={props.id} className={"size-12 rounded-lg flex justify-center flex-col shadow-md" + " " + "bg-[" + props.color + "]"}>
     <span className="material-symbols-outlined text-center select-none">
       {props.icon}
     </span>
   </div >;
 }
 
+function Divider() {
+  return <div id="line" className="w-[80vw] backdrop-blur-md bg-white bg-opacity-20 shadow-md h-1 mx-auto my-10" />;
+}
+
+function Headlogo() {
+  return <div id="logodiv" className="flex self-center items-center justify-items-center justify-self-center text-center pt-7 flex-row justify-center" >
+    <p id="logo" className={`${kranky.className} truncate normal text-6xl select-none text-white drop-shadow-md`}>ADHDO</p>
+  </div>;
+}
+
 export default function site() {
   return (
     <div id="site">
-      <div id="logodiv" className="flex self-center items-center justify-items-center justify-self-center text-center p-7 flex-row justify-center" >
-        <p id="logo" className={`${kranky.className} mx-10 truncate normal text-6xl select-none text-white drop-shadow-md`}>ADHDO</p>
-      </div>
-      <div id="line" className="w-[80vw] backdrop-blur-md bg-white bg-opacity-20 shadow-md h-1 mx-auto" />
-      <div id="actions" className="flex flex-row h-auto w-[100vw] justify-center justify-self-center mt-10">
+      <Headlogo />
+      <Divider />
+      <div id="actions" className="flex flex-row h-auto w-[100vw] justify-center justify-self-center">
         <Action icon="add" id="add" color="#722f37" />
       </div>
     </div>
