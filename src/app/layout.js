9b46d48a9654ebcf6aab61240eccd799d54370de
Joran Hennion <joran.hennion@gmail.com>
2024-10-11 21:07:21 +0200

    commit to test on website mobile

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <meta name="darkreader-lock"></meta>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
       </head>
       <body
