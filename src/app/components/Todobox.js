25eb5a7a4d4c174061787efd3ea4d15dce7844cd
Joran Hennion <joran.hennion@gmail.com>
2024-10-16 22:26:13 +0200

    fixed todo box a bit(still not styled tho)

--- src/app/components/Todobox.js
+++ src/app/components/Todobox.js
@@ -1,7 +1,18 @@
+function Todoitem(props) {
+  return (
+    <td>
+      <div className="justify-left mx-5 flex items-center">
+        <input type="checkbox" className="size-5" />
+        <p className="ml-5 text-center group-checked:opacity-0">a todo</p>
+      </div>
+    </td>
+  );
+}
+
 export default function Todobox(props) {
   return (
     <div className="m-10 table-auto rounded-lg bg-black bg-opacity-30 p-5 portrait:rounded-md">
-      <table id="todobox" class="h-full w-full table-auto text-white">
+      <table id="todobox" class="m-auto h-auto w-auto table-auto text-white">
         <thead>
           <tr>
             <th></th>
@@ -11,64 +22,19 @@ export default function Todobox(props) {
         </thead>
         <tbody>
           <tr>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
+            <Todoitem />
+            <Todoitem />
+            <Todoitem />
           </tr>
           <tr>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
+            <Todoitem />
+            <Todoitem />
+            <Todoitem />
           </tr>
           <tr>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
-            <td>
-              <div className="justify-left flex items-center gap-3">
-                <input type="checkbox" className="size-5" />
-                <p className="group-checked:opacity-0">a todo</p>
-              </div>
-            </td>
+            <Todoitem />
+            <Todoitem />
+            <Todoitem />
           </tr>
         </tbody>
       </table>
