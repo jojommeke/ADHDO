ce28182b67deeeab294b027436c568416c8d2d7f
Joran Hennion <joran.hennion@gmail.com>
2024-10-23 22:02:08 +0200

    added robots.txt

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://jojommeke.github.io/ADHDO/sitemap.xml",
+  };
+}
