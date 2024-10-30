6c713eac5967ef24782376f11e12ab86052db608
Joran Hennion <joran.hennion@gmail.com>
2024-10-30 22:19:50 +0100

    styled checkboxes(yes i do know it is not centered, it just won't listen)

---
+++ src/app/components/Checkbox.js
@@ -0,0 +1,37 @@
+// source = "https://www.material-tailwind.com/docs/html/checkbox#checkbox-with-label"
+
+export default function Checkbox(props) {
+  return (
+    <div class="inline-flex items-center">
+      <label class="relative flex cursor-pointer items-center" for={props.id}>
+        <input
+          type="checkbox"
+          class="peer h-5 w-5 cursor-pointer appearance-none rounded border border-slate-300 shadow transition-all checked:border-slate-800 checked:bg-blue-800 hover:shadow-md"
+          id={props.id}
+        />
+        <span class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform text-white opacity-0 peer-checked:opacity-100">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="h-3.5 w-3.5"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            stroke="currentColor"
+            stroke-width="1"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </span>
+      </label>
+      <label
+        class="ml-2 mr-auto cursor-pointer text-sm text-white"
+        for={props.id}
+      >
+        {props.title}
+      </label>
+    </div>
+  );
+}
--- src/app/components/Todobox.js
+++ src/app/components/Todobox.js
@@ -1,46 +1,38 @@
+import Checkbox from "./Checkbox.js";
+
+// i know the math.random is a goofy solution
 function Todoitem(props) {
   return (
-    <td>
-      <div className="justify-left mx-5 flex items-center">
-        <input type="checkbox" className="size-5" />
-        <p className="ml-5 text-center group-checked:opacity-0">a todo</p>
-      </div>
-    </td>
+    <Checkbox
+      title="this is the title of a todo"
+      id={Math.floor(Math.random() * 100000)}
+    />
   );
 }
 
 export default function Todobox(props) {
   return (
-    <div className="m-10 table-auto rounded-lg bg-black bg-opacity-30 p-5 portrait:rounded-md">
-      <table
+    <div className="m-10 rounded-lg bg-black bg-opacity-30 p-5 portrait:rounded-md">
+      <div
         id="todobox"
-        className="m-auto h-auto w-auto table-auto text-white"
+        className="align-center flex h-auto w-auto items-center justify-center text-white"
       >
-        <thead>
-          <tr>
-            <th></th>
-            <th></th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <Todoitem />
-            <Todoitem />
-            <Todoitem />
-          </tr>
-          <tr>
-            <Todoitem />
-            <Todoitem />
-            <Todoitem />
-          </tr>
-          <tr>
-            <Todoitem />
-            <Todoitem />
-            <Todoitem />
-          </tr>
-        </tbody>
-      </table>
+        <div className="align-center w-auto flex-col items-center justify-center">
+          <Todoitem />
+          <Todoitem />
+          <Todoitem />
+        </div>
+        <div className="align-center w-auto flex-col items-center justify-center">
+          <Todoitem />
+          <Todoitem />
+          <Todoitem />
+        </div>
+        <div className="align-center w-auto flex-col items-center justify-center">
+          <Todoitem />
+          <Todoitem />
+          <Todoitem />
+        </div>
+      </div>
     </div>
   );
 }
